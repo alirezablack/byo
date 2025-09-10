fef16f0836fae1cf8f1f7214c85e2c27d7a9ea01
Ali.M <alimblack787878@gmail.com>
2025-09-10 22:59:14 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from telethon import TelegramClient, functions
 from telethon.sessions import StringSession
 import asyncio, datetime
 from flask import Flask
+import threading
 
 # --------------------
-api_id = 12128667  # api_id خودت
-api_hash = "ff5f1b17331d23aa0329ced7f255d408"  # api_hash خودت
+api_id = 12128667
+api_hash = "ff5f1b17331d23aa0329ced7f255d408"
 string = "1BJWap1sBu3vVPj6cwAjRVwxK-2Mbc9qw8VKzSyUk2QnWWHEuQHOYprYRX4z6GhHEph3mJ1hlzkCKNRYduZfeQM0n-JNIEx7qgWhoLRHcozuhLhDEOdKPdtxrcTiv0rm2Yz5KHYQAe6ZVdHpLbRC73QvQJsnVq-fiQDp5HH-wZpp19YOPpi9KAV6YTb5M1mJlnRdD8sA3dLnbzuALDSqgNkd_IbQY2Bv8Fe-SNSkLbyIfzKeY26ERLLbwAZnhXrmEk1OZTcpQfO6vna49RzD1qMMprJ0P4R2AS-4Akl-wwjCtBKXHdb-GFN594COSIblxmscnqJUcx0NhRgUr_M-8O_NtsMNlWmI="
 # --------------------
 
@@ -24,13 +25,21 @@ async def update_bio():
             print(f"بیو آپدیت شد به: {now}")
         except Exception as e:
             print("❌ خطا:", e)
-        await asyncio.sleep(60)  # هر ۶۰ ثانیه
+        await asyncio.sleep(60)
 
-async def main():
+async def runner():
     await client.start()
     await update_bio()
 
+def start_loop():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(runner())
+
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(main())
+    # اجرای حلقه تلگرام در یک ترد جدا
+    t = threading.Thread(target=start_loop)
+    t.start()
+
+    # اجرای Flask برای Render
     app.run(host="0.0.0.0", port=10000)
